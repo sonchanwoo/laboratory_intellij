eb437a4751df1348ff6920bf2d089f6cbff46893
sonchanwoo <chanchan1116@naver.com>
2022-02-25 14:56:45 +0900

    initial

---
+++ src/main/java/com/example/laboratoryidea/LaboratoryIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.laboratoryidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryIdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laboratoryidea/LaboratoryIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratoryidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
